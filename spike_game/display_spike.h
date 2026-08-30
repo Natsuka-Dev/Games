fbdb1aa9c449ab070e9a673760810ade3b4d7fbf
Jacques Genin <jacquesgenin22@gmail.com>
2026-08-30 20:42:42 +0200

    ncurses spike game (my creation)

---
+++ spike_game/display_spike.h
@@ -0,0 +1,7 @@
+#pragma once
+
+void display_first_pyramid(int change, char tab[10][135]);
+void display_second_pyramid(int change, char tab[10][135], int key, int* pass);
+void fourty_two(char tab[10][135]);
+
+// fonctions declarées mais non définies
