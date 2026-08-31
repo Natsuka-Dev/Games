318473395f5618103ea5f2068157371d2fe98383
Jacques Genin <jacquesgenin22@gmail.com>
2026-08-31 21:55:48 +0200

    GAME_PRESENTATION message update

--- minesweeper/main.c
+++ minesweeper/main.c
@@ -3,7 +3,7 @@
 
 #include "all.h"
 
-#define GAME_PRESENTATION "You can move thanks to your keyboard arrows\n\n'f' -> add a flag (or remove it)\n'Space' -> Click\n'Q' -> Quit\n\nPress any Key, Good luck!\n\n\n\nYou can also create a personalised map!\n\nEx: ./minesweeper A B C\nA = lenght\nB = width\nC = percentage of mines\n\nLenght and width must be between 9 and 100(included).\nPercentage of mines must be between 5 and 40 (included).\n"
+#define GAME_PRESENTATION "Welcome to my own version of the Minesweeper!\n\n'Keyboard arrows' -> Move accross the map\n'f' -> Add a flag (or remove it)\n'Space' -> Click\n'Q' -> Quit\n\n\nYou can also create personalised maps!\n\nEx: ./minesweeper A B C\nA = lenght\nB = width\nC = percentage of mines\n\nLenght and width must be between 9 and 100(included).\nPercentage of mines must be between 5 and 40 (included).\n\nPress any Key, hope you enjoy it!\n"
 #define YOU_LOST "\nSorry but you failed! Better luck next time!\n"
 #define YOU_WON(a, b) "\nCongratulations! you managed to do a %d x %d minesweeper!\n", a, b
 
